66336f63cc104136e5895e75afa0de8565cd74c6
SwapnilPande <swapnil.pande98@gmail.com>
2023-10-31 11:49:51 -0400

    Added setup for mDNS discovery

--- install.sh
+++ install.sh
@@ -7,12 +7,18 @@ sudo apt-get update
 sudo apt-get install -y avahi-daemon
 
 # Download and install Miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 rm miniconda.sh
 
-# Optionally, you might want to add Miniconda to PATH, or do this manually later
+# Optionally, add Miniconda to PATH, or do this manually later
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+
+# Setup Avahi service definition
+sudo cp assets/test_stand.service /etc/avahi/services/
+
+# Restart Avahi
+sudo systemctl restart avahi-daemon
 
 echo "Installation complete."
